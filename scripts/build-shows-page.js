5d384e782493a655c4bf05d43f219a10d81204e4
Graciela Manzanares <gracielamanzanares@MacBook-Pro.localdomain>
2023-07-12 10:23:00 -0400

    added assets for sprint-2, added iframe for soundcloud song in pages.html

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,43 @@
+
+let date = 'DATE';
+let venue = 'VENUE';
+let location = 'LOCATION';
+
+let concertsData = [
+    {
+        title: 'Mon Sept 06 2021',
+        description: 'Ronald Lane',
+        isComplete: 'San Francisco, CA'
+    },
+    {
+        title: 'Tue Sept 21 2021',
+        description: 'Pier 3 East',
+        isComplete: 'San Francisco, CA'
+    },
+    {
+        title: 'Fri Oct 15 2021',
+        description: 'View Lounge',
+        isComplete: 'San Francisco, CA'
+    },
+    {
+        title: 'Sat Nov 06 2021',
+        description: 'Hyatt Agency',
+        isComplete: 'San Francisco, CA'
+    },
+    {
+        title: 'Fri Nov 26 2021',
+        description: 'Moscow Center',
+        isComplete: 'San Francisco, CA'
+    },
+    {
+        title: 'Wed Dec 15 2021',
+        description: 'Press Club',
+        isComplete: 'San Francisco, CA'
+    }
+]
+    
+            
+            
+            
+            
+            
