317daf67159ce1ae799cc24b6ab55db037b5df52
Graciela Manzanares <gracielamanzanares@MacBook-Pro.local>
2023-07-13 23:38:02 -0400

    updated js script files and form in html

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -3,41 +3,66 @@ let date = 'DATE';
 let venue = 'VENUE';
 let location = 'LOCATION';
 
-let concertsData = [
+const shows = [
     {
-        title: 'Mon Sept 06 2021',
-        description: 'Ronald Lane',
-        isComplete: 'San Francisco, CA'
+        date: 'Mon Sept 06 2021',
+        venue: 'Ronald Lane',
+        location: 'San Francisco, CA'
     },
     {
-        title: 'Tue Sept 21 2021',
-        description: 'Pier 3 East',
-        isComplete: 'San Francisco, CA'
+        date: 'Tue Sept 21 2021',
+        venue: 'Pier 3 East',
+        location: 'San Francisco, CA'
     },
     {
-        title: 'Fri Oct 15 2021',
-        description: 'View Lounge',
-        isComplete: 'San Francisco, CA'
+        date: 'Fri Oct 15 2021',
+        venue: 'View Lounge',
+        location: 'San Francisco, CA'
     },
     {
-        title: 'Sat Nov 06 2021',
+        date: 'Sat Nov 06 2021',
         description: 'Hyatt Agency',
-        isComplete: 'San Francisco, CA'
+        location: 'San Francisco, CA'
     },
     {
-        title: 'Fri Nov 26 2021',
+        date: 'Fri Nov 26 2021',
         description: 'Moscow Center',
-        isComplete: 'San Francisco, CA'
+        location: 'San Francisco, CA'
     },
     {
-        title: 'Wed Dec 15 2021',
-        description: 'Press Club',
-        isComplete: 'San Francisco, CA'
+        date: 'Wed Dec 15 2021',
+        venue: 'Press Club',
+        location: 'San Francisco, CA'
     }
-]
+];
     
+function createShowCard(show) {
+    //create article with class of shows
+    const cardEl = document.createElement('article');
+    cardEl.classList.add('shows');
+    // create heading with class of shows__heading and add the inner text of 
+    const heading = document.createElement('h2');
+    heading.classList.add('shows__heading');
+    heading.innerText = 'Shows';
+
+    //create a ul with class of shows__header-list
+    const list = document.createElement('ul');
+    list.classList.add('shows__list');
+
+   
+
+
+
+    // append our elements above as children to the cardEl
+    cardEl.appendChild(heading);
+    cardEl.appendChild(list);
+   list.apppendChild(istItem);
+
+    return cardEl;
+} 
+
+function renderShows() {
             
-            
-            
-            
+      
+}          
             
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,5 +1,79 @@
+//array with 3 default comment objects to start. comments have a name, timestamp and comment text.
 const comments = [
-    {name: 'Connor Walton', date: '02/17/2021', comment:'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.' },
-    {name: 'Emilie Beach', date: '01/09/2021', comment: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.'},
-    {name: 'Miles Acosta', date: '12/20/2020', comment: `I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.`}
-]
+    {name: 'Connor Walton', timestamp: '02/17/2021', note:'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.' },
+    {name: 'Emilie Beach', timestamp: '01/09/2021', note: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.'},
+    {name: 'Miles Acosta', timestamp: '12/20/2020', note: `I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.`}
+];
+
+//function that takes in one comment object as a parameter and displays it on the page
+
+
+function createCommentCard(comment){
+    // Create article with class of comments
+      const cardEl = document.createElement('article');
+      cardEl.classList.add('comments__card');
+
+    // create paragraph elements for the name and note and span html element 
+    //for date and add the inner text of comment.name, comment.timestamp and comment.note
+    const name = document.createElement('p');
+    cardEl.classList.add('comments__card');
+    name.innerText = comment.name;
+    const timestamp = document.createElement('span');
+    cardEl.classList.add('comments__card');
+    timestamp.innerText = comment.timestamp;
+    const note = document.createElement('p');
+    cardEl.classList.add('comments__card');
+    note.innerText = comment.note;
+
+    cardEl.appendChild(name);
+    cardEl.appendChild(timestamp);
+    cardEl.appendChild(note);
+
+    return cardEl;
+}
+
+function renderAppointments() {
+    // Grab #my-appointments from html to append cards below
+    const myAppointmentsEl = document.querySelector("#my-appointments");
+
+    // Clear the appointments div if appointments are already there.
+    myAppointmentsEl.innerHTML = "";
+
+    // Use forEach to loop through each item in our appointment array and create a card
+    appointments.forEach(appointment => {
+        const card = createAppointmentCard(appointment);
+        myAppointmentsEl.appendChild(card);
+    })
+}
+
+//Selecting '.comments__button' with querySelector and save as button
+const button = document.querySelector('.comments__button');
+
+
+//Created a const variable formEl to store the value of '#comments__form' from the DOM
+const formEl = document.querySelector('#comments__form');
+//Created a handleSubmit function that takes in the event object
+
+
+// 11. Create a variable called lastName and store the value of the event target lastname
+// 12. Create a variable called myNameEl Store the element '#my-name' from the dom using query selector
+// 13. Change the innerText of myNameEl concatenating the firstName variable and lastName
+
+
+
+const handleSubmit = (event) => {
+    event.preventDefault(); // Prevents the form using its default behavior
+    
+    const fullName = event.target.fullName.value; //Create a variable called fullName and store the value of the event target fullName
+    const comment = event.target.comment.value;
+    
+    const myNameEl = document.querySelector('#comments__display');
+    myNameEl.innerText = fullName;
+
+
+    event.target.reset(); //Reset the form 
+
+    formEl.removeEventListener("submit", handleSubmit); //Remove event listener
+};
+
+formEl.addEventListener("submit", handleSubmit);
