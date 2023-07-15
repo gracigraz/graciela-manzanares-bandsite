7c6f15e94aca239a77c723e7a38ece2fd6cb1eb7
Graciela Manzanares <gracielamanzanares@MacBook-Pro.local>
2023-07-15 13:39:52 -0400

    updated JS form in bio page, scss/js shows page and grouped main nav and footer scss in global scss page

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,8 +1,4 @@
 
-let date = 'DATE';
-let venue = 'VENUE';
-let location = 'LOCATION';
-
 const shows = [
     {
         date: 'Mon Sept 06 2021',
@@ -35,34 +31,99 @@ const shows = [
         location: 'San Francisco, CA'
     }
 ];
-    
+
+
+
+
+ //create article with class of shows
+const section = document.createElement('section');
+section.classList.add('shows');
+
+
+ //get header
+ const headerHtml = document.querySelector('header')
+ headerHtml.appendChild(main);
+ 
+  //create main html tag
+ const main = document.createElement('main');
+ main.appendChild(section);
+
+  // create heading with class of shows__heading and add the inner text of 
+const heading = document.createElement('h2');
+heading.classList.add('shows__heading');
+heading.innerText = 'Shows';
+
+section.appendChild(heading);
+
 function createShowCard(show) {
     //create article with class of shows
-    const cardEl = document.createElement('article');
-    cardEl.classList.add('shows');
-    // create heading with class of shows__heading and add the inner text of 
-    const heading = document.createElement('h2');
-    heading.classList.add('shows__heading');
-    heading.innerText = 'Shows';
+    const showCardEl = document.createElement('article');
+    showCardEl.classList.add('shows__card');
 
-    //create a ul with class of shows__header-list
-    const list = document.createElement('ul');
-    list.classList.add('shows__list');
+    // create h3 elements for the date, venue and location table headers
+    const dateHeader = document.createElement('h3');
+    dateHeader.classList.add('shows__header-date');
+    dateHeader.innerText = "DATE";
 
-   
+    const venueHeader = document.createElement('h3');
+    venueHeader.classList.add('shows__header-venue');
+    venueHeader.innerText = "VENUE";
 
+    const locationHeader = document.createElement('h3');
+    locationHeader.classList.add('shows__header-location');
+    locationHeader.innerText = "LOCATION";
 
+    // create paragraph elements for the date, venue and location
+    const date = document.createElement('p');
+    date.classList.add('shows__date');
+    date.innerText = show.date;
+  
+    const venue = document.createElement('p');
+    venue.classList.add('shows__venue');
+    venue.innerText = show.venue;
+  
+    const location = document.createElement('p');
+    location.classList.add('shows__location');
+    location.innerText = show.location;
 
-    // append our elements above as children to the cardEl
-    cardEl.appendChild(heading);
-    cardEl.appendChild(list);
-   list.apppendChild(istItem);
+    const button = document.createElement('button');
+    button.classList.add('shows__button');
+    button.innerText = "BUY TICKETS";
+
+    // append  elements above as children to the cardEl
+    showCardEl.appendChild(heading);
+    showCardEl.appendChild(dateHeader);
+    showCardEl.appendChild(venueHeader);
+    showCardEl.appendChild(locationHeader);
+    showCardEl.appendChild(date);
+    showCardEl.appendChild(venue);
+    showCardEl.appendChild(location);
 
     return cardEl;
 } 
+
+section.appendChild(cardEl);
 
 function renderShows() {
             
       
 }          
-            
+    
+ // Loop through the todo items and append a list item for each of them       
+for (let i=0; i<shows.length;i++){
+    let itemEl = document.createElement('li');
+    itemEl.innerText = shows[i].date;
+    itemEl.classList.add('todo-list__item');
+    todoListEl.appendChild(itemEl);
+}
+
+    // //create a ul with class of shows__list
+    // const list = document.createElement('ul');
+    // list.classList.add('shows__list');
+
+    // // //create a li with class of shows__list-item
+    // const listItem = document.createElement('li');
+    // list.classList.add('shows__list-item');
+
+    // showCardEl.appendChild(list);
+    // list.apppendChild(listItem);
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,79 +1,124 @@
 //array with 3 default comment objects to start. comments have a name, timestamp and comment text.
-const comments = [
+const commentsF = [
     {name: 'Connor Walton', timestamp: '02/17/2021', note:'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.' },
     {name: 'Emilie Beach', timestamp: '01/09/2021', note: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.'},
     {name: 'Miles Acosta', timestamp: '12/20/2020', note: `I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.`}
 ];
 
-//function that takes in one comment object as a parameter and displays it on the page
-
-
 function createCommentCard(comment){
     // Create article with class of comments
-      const cardEl = document.createElement('article');
-      cardEl.classList.add('comments__card');
+    const cardEl = document.createElement('article');
+    cardEl.classList.add('comments__card');
+
+    //create image for avatar with class of comments__img
+    const imageEl = document.createElement('div');
+    imageEl.classList.add('comments__avatar');
 
     // create paragraph elements for the name and note and span html element 
     //for date and add the inner text of comment.name, comment.timestamp and comment.note
     const name = document.createElement('p');
-    cardEl.classList.add('comments__card');
+    name.classList.add('comments__name');
     name.innerText = comment.name;
+
     const timestamp = document.createElement('span');
-    cardEl.classList.add('comments__card');
+    timestamp.classList.add('comments__tiemstamp');
     timestamp.innerText = comment.timestamp;
+
     const note = document.createElement('p');
-    cardEl.classList.add('comments__card');
+    note.classList.add('comments__note');
     note.innerText = comment.note;
 
-    cardEl.appendChild(name);
-    cardEl.appendChild(timestamp);
-    cardEl.appendChild(note);
-
+    //create div and add a class of comments__header-div to use flexbox on the name and date
+    const divHeader = document.createElement('div');
+    divHeader.classList.add('comments__header-div');
+    
+    //create div and add a class of comments__body-div to use flexbox on the header-div (containing name and date) and the comment itsef
+    const divBody = document.createElement('div');
+    divBody.classList.add('comments__body-div');
+ 
+     // append elements above as children to the cardEl
+    cardEl.appendChild(imageEl);
+    cardEl.appendChild(divBody);
+    divBody.appendChild(divHeader)
+    divBody.appendChild(note)
+    divHeader.appendChild(name);
+    divHeader.appendChild(timestamp);
+    
     return cardEl;
 }
 
-function renderAppointments() {
-    // Grab #my-appointments from html to append cards below
-    const myAppointmentsEl = document.querySelector("#my-appointments");
+const section = document.querySelector('.comments');
+const cardCon = document.createElement('div');
+cardCon.classList.add('comments__div'); 
+section.appendChild(cardCon);
 
-    // Clear the appointments div if appointments are already there.
-    myAppointmentsEl.innerHTML = "";
-
-    // Use forEach to loop through each item in our appointment array and create a card
-    appointments.forEach(appointment => {
-        const card = createAppointmentCard(appointment);
-        myAppointmentsEl.appendChild(card);
-    })
+//function that takes in one comment object as a parameter and displays it on the page
+function displayComment(comments) {
+    // Grab comments section (class = "comments") from html to append comment cards below
+    const commentsEl = document.querySelector('.comments__div');
+    commentsEl.innerHTML = ""; 
+    // Use forEach to loop through each item in our commentsF array and create a card
+    comments.forEach(comment => {
+        const card = createCommentCard(comment);
+        commentsEl.appendChild(card);
+    });
 }
 
-//Selecting '.comments__button' with querySelector and save as button
-const button = document.querySelector('.comments__button');
 
 
-//Created a const variable formEl to store the value of '#comments__form' from the DOM
-const formEl = document.querySelector('#comments__form');
-//Created a handleSubmit function that takes in the event object
-
-
-// 11. Create a variable called lastName and store the value of the event target lastname
-// 12. Create a variable called myNameEl Store the element '#my-name' from the dom using query selector
-// 13. Change the innerText of myNameEl concatenating the firstName variable and lastName
-
-
-
+//Create a handleSubmit function that takes in the event object
 const handleSubmit = (event) => {
     event.preventDefault(); // Prevents the form using its default behavior
     
     const fullName = event.target.fullName.value; //Create a variable called fullName and store the value of the event target fullName
-    const comment = event.target.comment.value;
-    
-    const myNameEl = document.querySelector('#comments__display');
-    myNameEl.innerText = fullName;
+    const message = event.target.message.value; //Create a variable called message and store the value of the event target fullName
+    // const timestamp = new Date().toLocaleDateString();
+    console.log(fullName);
+    console.log(message);
 
+      // Clean up data to push into and match our appointments array
+    const cardData = {
+        name: event.target.fullName.value,
+        timestamp: new Date().toLocaleDateString(),
+        note: event.target.message.value
+    };
+    console.log(cardData)
+    commentsF.push(cardData);
+    console.log(commentsF);
+    event.target.reset(); //Reset the form
+ 
+    displayComment(commentsF);
 
-    event.target.reset(); //Reset the form 
-
-    formEl.removeEventListener("submit", handleSubmit); //Remove event listener
 };
-
+//Creat a const variable formEl to store the value of '#comments__form' from the DOM
+const formEl = document.querySelector('#comments__form');
+// Add an event listener to the form to get all input data within form
 formEl.addEventListener("submit", handleSubmit);
+
+
+displayComment(commentsF);
+
+
+
+// //Selecting '.comments__button' with querySelector and save as button
+// const button = document.querySelector('.comments__button');
+
+// // Add event listener, click, to button variable 
+// button.addEventListener("click", (event) => {
+//     console.log(event);
+//     console.log(`Button clicked at ${event.clientX}, ${event.clientY}`);
+// });
+
+// //Create const variables called inputElName and inputElNote to retrieve the input field and store it as the value
+// const inputElName = document.querySelector('.comments__input-name');
+// const inputElNote = document.querySelector('.comments__input-comment');
+
+// //  Add an event listener of 'input' in the callback log the targets value.
+// inputElName.addEventListener("input", (event) => {
+//     console.log(event.target.value);
+// });
+// inputElNote.addEventListener("input", (event) => {
+//     console.log(event.target.value);
+// });
+
+
